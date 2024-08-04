15f408dd18f7c97506533908a3968ac4a830e91f
PeaceDumss <peacedumeda369@gmail.com>
2024-08-04 06:16:29 +0000

    Peace_HyperSdk

--- consts/consts.go
+++ consts/consts.go
@@ -13,11 +13,11 @@ import (
 
 const (
 	// TODO: choose a human-readable part for your hyperchain
-	HRP = ""
+	HRP = "PeaceHyperChain"
 	// TODO: choose a name for your hyperchain
-	Name = ""
+	Name = "Peace"
 	// TODO: choose a token symbol
-	Symbol = ""
+	Symbol = "P"
 )
 
 var ID ids.ID
